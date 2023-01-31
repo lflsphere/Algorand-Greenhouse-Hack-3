f38e91802551e35996dd917ef81f50344e64520f
LFL <lfl@numericable.fr>
2023-01-31 20:14:12 +0100

    maj flow

--- SC/flow.py
+++ SC/flow.py
@@ -7,7 +7,7 @@ from typing import Final
 
 APP_CREATOR = Seq(creator := AppParam.creator(Int(0)), creator.value())
 
-class Auction(Application):
+class Flow(Application):
 
     """
     receivers: Final[AccountStateValue] = AccountStateValue(
@@ -20,20 +20,8 @@ class Auction(Application):
     """ 
 
     # Global State
-    # - Highest Bidder: The address of the highest bidder
+    # - durée d'un mois en secondes
     one_month_time: Final[ApplicationStateValue] = ApplicationStateValue(stack_type=TealType.uint64, default=Int(Int(2629800)))
-
-    # - Highest Bid: The amount of the highest bid
-    highest_bid: Final[ApplicationStateValue] = ApplicationStateValue(stack_type=TealType.uint64, default=Int(0))
-
-    # - Auction End: The timestamp when the auction ends
-    auction_end: Final[ApplicationStateValue] = ApplicationStateValue(stack_type=TealType.uint64, default=Int(0))
-
-    # - ASA: The ASA being auctioned
-    asa: Final[ApplicationStateValue] = ApplicationStateValue(stack_type=TealType.uint64, default=Int(0))
-
-    # - ASA Amount: The amount of ASA being auctioned
-    asa_amount: Final[ApplicationStateValue] = ApplicationStateValue(stack_type=TealType.uint64, default=Int(0))
 
 
     # Create Application
@@ -41,6 +29,7 @@ class Auction(Application):
     def create(self):
         return self.initialize_application_state()
 
+    """
     # Opt app into ASA
     @external(authorize=Authorize.only(APP_CREATOR))
     def opt_into_asset(self, asset: abi.Asset):
@@ -55,6 +44,7 @@ class Auction(Application):
                 TxnField.asset_amount: Int(0)
             })
         )
+    """
 
     @internal(TealType.none)
     def pay(self, receiver, amount):
@@ -69,7 +59,7 @@ class Auction(Application):
     def pay_by_tx_sender(self, receiver, amount):
         return InnerTxnBuilder.Execute({
             TxnField.type_enum: TxnType.Payment,
-            TxnField.sender : Txn.sender(), # devrait ne pas marcher car si le sender n'est pas le contrat alors le sender qu'on met doit avoir été rekeyed vers l'adresse du contrat
+            TxnField.sender : Txn.sender(), # devrait ne pas marcher car si le sender doit être le SC (cf. dev portal) MAIS on utilise la SS pour que le SC puisse on-behalf du sender puisse payer le receiver
             TxnField.receiver: receiver,
             TxnField.amount: amount,
             TxnField.fee: Int(0)
@@ -81,27 +71,36 @@ class Auction(Application):
     @external
     def create_flow(self, sender: abi.Account, receiver: abi.Account, flowRate: abi.Uint64): #, axfer: abi.AssetTransferTransaction)
         return Seq(
-            # vérifier que la SS a été créée
-            first_month = Int(Mul(flowRate, one_month_time)), # doit être entier => check dans le back; sinon à voir pour la gestion des nbres décimaux 
-            self.pay_by_tx_sender(receiver = Global.current_application_address, first_month), # receiver = smart contract
 
+            # vérifier que la SS a été créée ou pas (en fait c'est peut être pas très grave)
             Assert(App.box_create(Bytes(Concat(sender.address(), receiver.address())), Int(24))), # il faut pas que des méchants puissent créer des boxes de leur côté (=> vérif box_array)
+            
+            first_month = Int(Mul(flowRate, self.one_month_time.get())), 
+            # il va sans doute falloir d'abord faire payer le first_month puis créer le flow (cf. comment ligne 62) et donc verifier dans le create que le first month a été créé.
+            self.pay_by_tx_sender(Global.current_application_address, first_month), # receiver = smart contract ou utiliser la SS ici plutôt
+
             App.box_put(Bytes(Concat(sender.address(), receiver.address())), Bytes(Concat(Itob(flowRate), Itob(Global.latest_timestamp), Itob(first_month))))
             
         )
 
     @external(read_only = True)
     def read_flow_rate(self, sender: abi.Account, receiver: abi.Account, *, output: Bytes): # on ne vérifie pas que la box existe
-        return output.set(App.box_extract(Bytes(Concat(sender.address(), receiver.address()), Int(1), Int(8))))
+        return output.set(App.box_extract(Bytes(Concat(sender.address(), receiver.address()), Int(0), Int(8))))
 
     @external(read_only = True)
     def read_timestamp(self, sender: abi.Account, receiver: abi.Account, *, output: Bytes): # on ne vérifie pas que la box existe
-        return output.set(App.box_extract(Bytes(Concat(sender.address(), receiver.address()), Int(9), Int(8))))
+        return output.set(App.box_extract(Bytes(Concat(sender.address(), receiver.address()), Int(8), Int(8))))
     
+    @external(read_only = True)
+    def read_first_month(self, sender: abi.Account, receiver: abi.Account, *, output: Bytes): # on ne vérifie pas que la box existe
+        return output.set(App.box_extract(Bytes(Concat(sender.address(), receiver.address()), Int(16), Int(8))))
+    
+    """
+    on n'utilise plus de zone tampon
     @external(read_only = True)
     def read_former_payments(self, sender: abi.Account, receiver: abi.Account, *, output: Bytes): # on ne vérifie pas que la box existe
         return output.set(App.box_extract(Bytes(Concat(sender.address(), receiver.address()), Int(17), Int(8))))
-
+    """
 
     # c'est que le sender qui update
     @external
@@ -112,9 +111,9 @@ class Auction(Application):
             self.asa_amount.set(axfer.get().asset_amount()),
             self.auction_end.set(length.get() + Global.latest_timestamp()),
             self.highest_bid.set(starting_price.get())
-            """
+           
 
-            """
+           
             Assert(self.auction_end.get() == Int(0)),
             Assert(axfer.get().asset_receiver() == Global.current_application_address()),
             Assert(axfer.get().xfer_asset() == self.asa.get()),
@@ -130,9 +129,9 @@ class Auction(Application):
             former_timestamp = Bytes(read_timestamp(sender, receiver)),
             former_payments = Bytes(read_former_payments(sender, receiver)),
             due_payment = BytesMul(Bytes("base16", former_flow_rate), Bytes("base16", former_timestamp)),
-            # payment_so_far = BytesAdd(Bytes("base16", last_payment), Bytes("base16", former_payments)),
+            # payment_so_far = BytesAdd(Bytes("base16", last_payment), Bytes("base16", former_payments)), plus de zone tampon
 
-            self.pay_by_tx_sender(receiver = Global.current_application_address, due_payment), # receiver = smart contract
+            self.pay_by_tx_sender(receiver = Global.current_application_address, due_payment), # receiver = smart contract ou utiliser la SS ici plutôt
 
 
             App.box_replace(Bytes(Concat(sender.address(), receiver.address())), Int(0), Bytes(Concat(Itob(new_flowRate), Itob(Global.latest_timestamp))))
@@ -144,7 +143,7 @@ class Auction(Application):
     @external
     def claim(self, sender: abi.Account, receiver: abi.Account):
         # faire l'appel à la SS 
-        # faire un claim de la somme latest_payment avec la SS comme ça pas besoin de zone tampon
+        # faire un claim de la somme latest_payment avec la SS comme ça pas besoin de zone tampon ?
 
 
     
@@ -152,7 +151,7 @@ class Auction(Application):
 
     
     
-
+    """
     # Bid
     # Place a new bid and return previous bid
     @external
@@ -199,6 +198,7 @@ class Auction(Application):
                 TxnField.asset_close_to: APP_CREATOR
             })
         )
+    """
 
     # Delete app
     # Send MBR funds to creator and delete app
